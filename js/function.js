c4a933571a9f41479da33132c476ee6c6a8172b5
andreykyr <GubernatorK@yandex.ru>
2025-10-09 21:49:15 +0300

    Подготовил функции

---
+++ js/function.js
@@ -0,0 +1,28 @@
+let checkLength = function (string, maxLength) {
+  return string.length <= maxLength;
+};
+
+let checkPolydrome = function (string) {
+  string = string.toLowerCase().replaceAll(' ','');
+  let result = true;
+  for (let i = 1; i < string.length / 2; i++) {
+    if (string.at(i - 1) !== string.at(-i)) {
+      result = false;
+      break;
+    }
+  }
+  return result;
+};
+
+let extractNumbers = function (string) {
+  string = String(string);
+  string = string.replaceAll(' ','');
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] <= 9) {
+      result += string[i];
+    }
+  }
+  return result ? Number(result) : NaN;
+};
+
